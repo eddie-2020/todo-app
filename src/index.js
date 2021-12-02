3ea9f90c37be47fc94803b9e86140fe671709908
Edward Yara <83566118+oluyaratosin123@users.noreply.github.com>
2021-12-02 16:19:56 +0100

    Add interactivity to To Do List

--- src/index.js
+++ src/index.js
@@ -1,41 +1,77 @@
 import './styles.css';
-import eventDisplayList from './functional';
+import { checkEvent, currentChecks } from './storage';
+import { setEventToLocalStorage, getEventFromLocalStorage } from './functional';
 
-export const eventList = document.querySelector('#event-data');
-const data = [
+// Array Of Event Tasks To Do
+const eventList = [
   {
     description: 'Play video games',
     completed: false,
+    index: 0,
+  },
+  {
+    description: 'Go Shopping',
+    completed: true,
     index: 1,
+  },
+  {
+    description: 'Cook Dinner',
+    completed: false,
+    index: 2,
   },
   {
     description: 'Listen to music',
     completed: true,
-    index: 2,
-  },
-  {
-    description: 'Go Shopping',
-    completed: false,
     index: 3,
   },
   {
-    description: 'Go to the gym',
-    completed: true,
+    description: 'Read Novels',
+    completed: false,
     index: 4,
   },
 ];
 
-export const events = data.sort((a, b) => {
-  const indexA = a.index;
-  const indexB = b.index;
-
-  if (indexA < indexB) {
-    return -1;
+// Add Events To List
+const addToEvent = () => {
+  if (getEventFromLocalStorage() === null) {
+    setEventToLocalStorage(eventList);
+  } else {
+    const sortedEventList = getEventFromLocalStorage().sort((a, b) => a.index - b.index);
+    sortedEventList.sort((x, y) => x.index - y.index);
+    for (let i = 0; i < sortedEventList.length; i += 1) {
+      if (sortedEventList[i].completed === true) {
+        checkEvent(sortedEventList[i].index, true);
+      } else {
+        checkEvent(sortedEventList[i].index, false);
+      }
+      // create list item
+      const listEventItems = document.querySelector('.eventList');
+      listEventItems.insertAdjacentHTML(
+        'beforeend',
+        `
+          <div class="eventTask">
+            <div class="eventChecks">
+              <input type="checkbox" name="item-${sortedEventList[i].index
+}" class="checkbox" ${sortedEventList[i].completed ? 'checked' : ''}>
+              <span class="eventCheckmark" ${sortedEventList[i].completed
+    ? 'style="text-decoration: line-through"'
+    : ''
+}>${sortedEventList[i].description}</span>
+            </div>
+            <div class="material-icons-outlined">more_vert</div>
+          </div>
+        `,
+      );
+      checkEvent();
+    }
   }
-  if (indexA > indexB) {
-    return 1;
-  }
-  return 0;
-});
+};
 
-window.addEventListener('load', eventDisplayList(events, eventList));
+window.onload = () => {
+  currentChecks(eventList);
+  addToEvent();
+  getEventFromLocalStorage(eventList);
+  setEventToLocalStorage(eventList);
+};
+
+export default { addToEvent };
