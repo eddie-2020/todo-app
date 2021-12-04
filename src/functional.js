5b0af68bc17d14921bfd631f7de59e18fa84ee1e
direakanbi <83566118+oluyaratosin123@users.noreply.github.com>
2021-12-04 14:36:09 +0100

    Added add() function and remove() function to my To Do List
    - Add full web App Interaction
    - Add Styles
    - Add to list and remove to list
    - Add LocalStorage

--- src/functional.js
+++
@@ -1,9 +0,0 @@
-const setEventToLocalStorage = (eventToDo) => {
-  console.log(eventToDo);
-  localStorage.setItem('eventToDo', JSON.stringify(eventToDo));
-};
-const getEventFromLocalStorage = () => {
-  const eventToDo = localStorage.getItem('eventToDo');
-  return JSON.parse(eventToDo);
-};
-export { setEventToLocalStorage, getEventFromLocalStorage };
